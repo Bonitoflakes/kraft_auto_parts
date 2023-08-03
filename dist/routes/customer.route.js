40a447241dec435494523caeee45a6f444fe618b
Rishab Khivsara <rishabjain3500@gmail.com>
2023-08-03 15:48:07 +0530

    customer CRUD  done

--- dist/routes/customer.route.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=customer.route.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
 import express from "express";
-import { getAllCustomers } from "./controllers/customer.controller";
+import { Router as customerRoute } from "./routes/customer.route";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.get("/health", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/customers", async (req, res) => {
-  const data = await getAllCustomers();
-  res.send(data);
-});
+app.use("/api", customerRoute);
 
 app.listen(3333, () => {
   console.log("Server is running on port 3333");
